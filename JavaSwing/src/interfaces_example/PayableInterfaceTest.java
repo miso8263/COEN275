f39d95e8129a209dc37245b012560150a82f4405
Michelle <mmsoult@gmail.com>
2016-07-11 19:40:11 -0700

    Add snippets of other examples

---
+++ JavaSwing/src/interfaces_example/PayableInterfaceTest.java
@@ -0,0 +1,12 @@
+package interfaces_example;
+
+public class PayableInterfaceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Payable[] payableObjects = new Payable[4];
+		
+		payableObjects[0] = new Employee("Farah", "Jacqueline", "222-22-2222", 1200.00);
+	}
+
+}
