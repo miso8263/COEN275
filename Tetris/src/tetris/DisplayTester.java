de80fd8670e2b7d53735397562941b3043ab769a
sindujab <sinduja2606@gmail.com>
2016-08-01 21:06:52 -0700

    display and display tester classes
    please review and let me know

---
+++ Tetris/src/tetris/DisplayTester.java
@@ -0,0 +1,21 @@
+package tetris;
+
+import java.awt.EventQueue;
+
+public class DisplayTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameDisplay window = new GameDisplay();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
