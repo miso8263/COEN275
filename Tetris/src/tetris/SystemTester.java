13e66e7e9ac4aa05f18228fede360f46fb8dfb7a
Vonyar <hoose@batnet.com>
2016-08-21 11:39:28 -0700

    Updated SystemTester to move the tetromino left and right as well as rotating both clockwise and counter-clockwise.

--- Tetris/src/tetris/SystemTester.java
+++ Tetris/src/tetris/SystemTester.java
@@ -49,7 +49,24 @@ public class SystemTester {
 
 		for (int i = 0; i < 20; i++)
 		{
+			//move tetromino down
 			tetrisSystem.moveActiveTetromino(0, -1, 0);
+/*			
+			//move tetromino left 4, right 9, left 4
+			if(i < 4 || i > 16){
+				tetrisSystem.moveActiveTetromino(-1, 0, 0);				
+			} 
+			if (i > 5 && i < 15){
+				tetrisSystem.moveActiveTetromino(1, 0, 0);
+			}
+*/
+			//rotate tetromino clockwise 10, counter-clockwise 10
+			if (i < 10){
+				tetrisSystem.moveActiveTetromino(0, 0, 1);
+			} else {
+				tetrisSystem.moveActiveTetromino(0, 0, -1);
+			}
+			
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
--- Tetris/src/tetris/TetrominoTest.java
+++ Tetris/src/tetris/TetrominoTest.java
@@ -6,6 +6,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TetrominoTest {
+	
+	
+	
+	// Function to see if two tetromino are equal
+	private boolean tetrominoEqual(Tetromino a, Tetromino b){
+		
+		boolean[][] aGrid = a.getShapeGrid();
+		boolean[][] bGrid = b.getShapeGrid();
+
+		int gridLength = aGrid.length;
+		
+		if (a.getXLocation() != b.getXLocation()) {
+			return false;
+		};
+		if (a.getYLocation() != b.getYLocation()){
+			return false;
+		};
+		for(int i=0; i < gridLength; i++) {
+			for(int j=0; j< gridLength; j++) {
+				if (aGrid[i][j] != bGrid[i][j]) {
+					return false;
+				};
+			}
+		}
+		return true;
+	}
 
 	@Before
 	public void setUp() throws Exception {
